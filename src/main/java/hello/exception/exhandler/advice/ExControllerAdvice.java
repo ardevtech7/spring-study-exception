c9b1e5afeca56cb368183ce6b1dcf7ff3b0e14e7
ardevtech7 <ardevtech7@gmail.com>
2023-01-16 18:53:29 +0900

    Spring Exception Study - @ControllerAdvice

--- src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
+++ src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
@@ -1,18 +1,17 @@
-package hello.exception.exception;
+package hello.exception.exhandler.advice;
 
-import hello.exception.api.ApiExceptionController;
 import hello.exception.exception.UserException;
 import hello.exception.exhandler.ErrorResult;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestController
-public class ApiExceptionV2Controller {
+@RestControllerAdvice(basePackages = "hello.exception.api")
+public class ExControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
@@ -35,24 +34,4 @@ public class ApiExceptionV2Controller {
         return new ErrorResult("EX", "내부 오류");
     }
 
-    @GetMapping("/api2/members/{id}")
-    public MemberDto getMember(@PathVariable String id) {
-        if (id.equals("ex")) {
-            throw new RuntimeException("잘못된 사용자");
-        }
-        if (id.equals("bad")) {
-            throw new IllegalArgumentException("잘못된 입력 값");
-        }
-        if (id.equals("user-ex")) {
-            throw new UserException("사용자 오류");
-        }
-        return new MemberDto(id, "hello" + id);
-    }
-
-    @Data
-    @AllArgsConstructor
-    static class MemberDto {
-        private String memberId;
-        private String name;
-    }
 }
